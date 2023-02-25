6738ad9112b645fb5f28655bdf96f112c9ebca6b
MarahShabello <s11819990@stu.najah.edu>
2023-02-25 20:07:15 +0200

    Dark Theme

--- switchTheme.js
+++ switchTheme.js
@@ -5,27 +5,24 @@ const themeMode = document.getElementsByClassName("theme-mode")[0];
 themeBtn.addEventListener("click", switchTheme);
 
 function switchTheme() {
-    if (localStorage.getItem("theme") === "light") {
-        document.body.classList.remove("light");
-        document.body.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-        console.log("Change to dark Mode");
-        themeIcon.classList.replace("bi-moon", "bi-sun");
-        themeMode.textContent = "Light Mode";
-    } else {
-        document.body.classList.remove("dark");
-        document.body.classList.add("light");
-        localStorage.setItem("theme", "light");
-        console.log("Change to light Mode");
-        themeIcon.classList.replace("bi-sun", "bi-moon");
-        themeMode.textContent = "Dark Mode";
+    let theme = localStorage.getItem("theme");
+    themeMode.textContent = `${theme.charAt(0).toUpperCase() + theme.slice(1)} Mode`;
+    if (theme === "dark"){
+        themeIcon.classList.replace("bi-sun", "bi-moon")
+        theme = "light"
     }
+    else {
+        themeIcon.classList.replace("bi-moon", "bi-sun")
+        theme = "dark"
+    }
+    document.body.className = theme;
+    localStorage.setItem("theme", theme);
 }
 
 window.onload = function checkTheme() {
-    const localStorageTheme = localStorage.getItem("theme");
-    if (localStorageTheme !== null && localStorageTheme === "dark") {
-        document.body.className = localStorageTheme;
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        document.body.className = theme;
         themeIcon.classList.replace("bi-moon", "bi-sun");
         themeMode.textContent = "Light Mode";
     }
